c7181d82a3cf3f594d60b051e8ee67bd92b59a5d
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-10 16:46:19 +0300

    u

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+from google.cloud import firestore
 import pandas as pd
 import yfinance as yf
 
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="port-test")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("assets").document("1")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
 st.title('Test')
+
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
+
+
+
