ec91f3be438c8ace058d584c3bd2ad4a24ec3e2a
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-10 16:53:09 +0300

    u

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import yfinance as yf
 
 import json
 key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
+creds = firestore.service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="port-test")
 
 # Create a reference to the Google post.
