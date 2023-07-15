1ab4afa4192c69f97dc6479dd12701a8a0e2b43d
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-15 18:06:19 +0300

    u

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 import pandas as pd
 import yfinance as yf
+import test as test
 
 import json
 key_dict = json.loads(st.secrets["textkey"])
@@ -19,6 +20,19 @@ st.title('Test')
 
 st.button('Add Asset')
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   test.lead()
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+   
 st.write("The id is: ", doc.id)
 st.write("The contents are: ", doc.to_dict())
 
