01c1be10e488ca89d451ebab94addef541906c36
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-15 21:20:51 +0300

    u

--- pages/Portfolio.py
+++ pages/Portfolio.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title('Portfolio')
 left, right = st.columns([4,1])
-left.selectbox('portfolio', ['All', 'USD', 'SAR'])
+left.selectbox('portfolio', ['All', 'USD', 'SAR'], label_visibility='collapsed')
 right.button('Add Portfolio')
 df = pd.DataFrame({'names': ['VTI', 'KSA'], 'Hold': [10, 12]})
 
