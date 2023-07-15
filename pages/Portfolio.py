f2e5069459c7bc0747549165164bff3c08945720
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-15 21:18:52 +0300

    u

--- pages/Portfolio.py
+++ pages/Portfolio.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
+st.title('Portfolio')
+st.selectbox('portfolio'. ['All', 'USD', 'SAR'])
 df = pd.DataFrame({'names': ['VTI', 'KSA'], 'Hold': [10, 12]})
 
 st.data_editor(df, num_rows='dynamic')
