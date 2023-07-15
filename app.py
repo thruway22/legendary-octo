8ff578ae948b224d7649a1736aa9744e52b5ce77
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-16 01:27:47 +0300

    u

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import yfinance as yf
 from db import db
 
 
-# Create a reference to the Google post.
-doc_ref = db.collection("assets").document("1")
+# # Create a reference to the Google post.
+# doc_ref = db.collection("assets").document("1")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# # Then get the data at that reference.
+# doc = doc_ref.get()
 
 st.title('Test')
    
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# st.write("The id is: ", doc.id)
+# st.write("The contents are: ", doc.to_dict())
 
 
 
--- pages/Portfolio.py
+++ pages/Portfolio.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import pandas as pd
+from db import db
 
 
 st.title('Portfolio')
-left, right = st.columns([4,1])
-left.selectbox('portfolio', ['All', 'USD', 'SAR'], label_visibility='collapsed')
-right.button('Add Portfolio')
-df = pd.DataFrame({'names': ['VTI', 'KSA'], 'Hold': [10, 12]})
+# left, right = st.columns([4,1])
+# left.selectbox('portfolio', ['All', 'USD', 'SAR'], label_visibility='collapsed')
+# right.button('Add Portfolio')
+# df = pd.DataFrame({'names': ['VTI', 'KSA'], 'Hold': [10, 12]})
 
-st.data_editor(df, num_rows='dynamic')
+dummy_data = {'name': 'X', 'currency': 'XXX'}
+pf_name = st.text_input('pf_name')
+pf_currency = st.text_input('pf_currency')
+submitted = st.button('Add Portfolio')
+
+col_ref = db.collection('portfolios')
+
+if submitted:
+    col_ref.document(pf_name).set(dummy_data)
