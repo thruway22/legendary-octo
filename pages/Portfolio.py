dbda85f4f3520781d9b5e67399c14ea5542d3fac
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-15 17:53:39 +0300

    u

--- pages/Portfolio.py
+++ pages/Portfolio.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
+
 
 st.write('portfolio')
+
+# df = pd.DataFrame({'Courses': pd.Series(dtype='str'),
+#                    'Fee': pd.Series(dtype='int'),
+#                    'Duration': pd.Series(dtype='str'),
+#                    'Discount': pd.Series(dtype='float')})
+
+df = pd.DataFrame({'Ticker', 'Holdings'})
+
+st.data_editor(df)
