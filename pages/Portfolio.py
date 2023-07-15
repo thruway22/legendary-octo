de6eeed339765f5e2a2fcad111512f09eb8fbb4b
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-07-15 21:19:06 +0300

    u

--- pages/Portfolio.py
+++ pages/Portfolio.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 st.title('Portfolio')
-st.selectbox('portfolio'. ['All', 'USD', 'SAR'])
+st.selectbox('portfolio', ['All', 'USD', 'SAR'])
 df = pd.DataFrame({'names': ['VTI', 'KSA'], 'Hold': [10, 12]})
 
 st.data_editor(df, num_rows='dynamic')
