1e3a8a3b42f33569bb51f9ed2801b7ff29da4a8e
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-07 00:34:14 +0300

    u

--- app.py
+++ app.py
@@ -15,5 +15,25 @@ st.title('Test')
 # st.write("The id is: ", doc.id)
 # st.write("The contents are: ", doc.to_dict())
 
+from streamlit_server_state import server_state, server_state_lock
+
+st.title("Global Counter Example")
+
+with server_state_lock["count"]:  # Lock the "count" state for thread-safety
+    if "count" not in server_state:
+        server_state.count = 0
+
+increment = st.button("Increment")
+if increment:
+    with server_state_lock.count:
+        server_state.count += 1
+
+decrement = st.button("Decrement")
+if decrement:
+    with server_state_lock.count:
+        server_state.count -= 1
+
+st.write("Count = ", server_state.count)
+
 
 
